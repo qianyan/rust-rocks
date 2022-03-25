ffad023be02d53a723a2c8649f73f7200dbce7b4
Yan Qian <qianyan.lambda@gmail.com>
2022-03-25 13:53:02 +0800

    feat(ownership): use RefCell for interior mutablity

--- 05-ownership/rc.rs
+++ 05-ownership/rc.rs
@@ -1,4 +1,5 @@
 use std::rc::Rc;
+use std::cell::RefCell;
 
 // Rust 如何创建不收栈内存控制的堆内存？
 // Box::leak() -> 从堆内存泄漏出去，不受栈内存控制。
@@ -17,10 +18,23 @@ fn main() {
 
     node1.update_downstream(Rc::new(node3));
     node2.update_downstream(node1.get_downstream().unwrap());
-    let node5 = Node::new(5);
-    let node3 = node1.get_downstream().unwrap();
-    node3.update_downstream(Rc::new(node5));
+//    let node5 = Node::new(5);
+//    let node3 = node1.get_downstream().unwrap();
+//    node3.update_downstream(Rc::new(node5));
+    borrow_with_ref_cell();
     println!("node1: {:?}, node2: {:?}", node1, node2);
+}
+
+fn borrow_with_ref_cell() {
+    let data = RefCell::new(1);
+    // 如果没有这个作用域，那么编译没有任何问题，但是在运行期间，会得到
+    // already mutably borrowed: BorrowError
+    {
+        let mut v = data.borrow_mut();
+        *v += 1;
+    }
+
+    println!("data: {:?}", data.borrow());
 }
 
 #[derive(Debug)]
@@ -45,3 +59,7 @@ impl Node {
         self.downstream.as_ref().map(|v| v.clone())
     }
 }
+
+// 外部可变性和内部可变性的区别 见Jk borrow_with_ref_cell
+// 1. 外部可用性：let mut 或者 &mut; 编译时，如果不符合规则，产生编译错误
+// 2. 内部可变性：使用 Cell / RefCell；运行时，如果不符合规则，产生 panic 错误
