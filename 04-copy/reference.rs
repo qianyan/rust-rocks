8870ae944f0cfe17b1a92b67292e1ba720977831
Yan Qian <qianyan.lambda@gmail.com>
2022-03-31 10:46:13 +0800

    fix(reference): fatpointer and ref, deref

--- 04-copy/reference.rs
+++ 04-copy/reference.rs
@@ -1,15 +1,24 @@
 //see https://blog.thoughtram.io/references-in-rust/
+use std::mem;
 fn main() {
     let data = vec![1, 2, 3, 4];
     let data_owned = &data;
+    // see &* meaning: https://stackoverflow.com/questions/41273041/what-does-combined-together-do-in-rust
+    // deref will recuirsivly deref, Vec<iu32> deref to &[T] and then to [T], so &*data deref to
+    // &[T]
     println!("addr of value: {:p}({:p}), addr of data {:p}, data_owned: {:p}, ptr of data: {:?}",
-        &data, data_owned, &&data, &data_owned, *&data);
+    &data, data_owned, &*data, &data_owned, data);
 
     println!("sum of data_owned: {}", sum(data_owned));
-
-    // 堆上数据的地址是什么？
+     // 堆上数据的地址是什么？
     println!("addr of items: [{:p}, {:p}, {:p}, {:p}]",
         &data[0], &data[1], &data[2], &data[3]);
+
+    //let p:[usize;3];
+    let p = unsafe { mem::transmute::<&Vec<u32>, usize>(&data) }; 
+    println!("stack value: {:x}",p); 
+
+
 }
 
 fn sum(data: &Vec<u32>) -> u32 {
@@ -18,10 +27,19 @@ fn sum(data: &Vec<u32>) -> u32 {
     data.iter().fold(0, |acc, x: &u32| acc + x)
 }
 
+fn inspect_mem() {
+    let data:Vec<u32> = vec![10, 42, 9, 8]; 
+    let p:[usize;3];
+    p = unsafe { mem::transmute(data) }; 
+    println!("stack value: {:x}, {:x}, {:x}",p[0], p[1], p[2]); 
+
+}
+
 //addr of value 指的是 data 这个胖指针的地址，他是堆上数据的所有者
 //data_owned->data, &data_owned是自己的地址。
-//addr of value: 0x7ff7bc8c4290(0x7ff7bc8c4290), addr of data 0x7ff7bc8c4350, data_owned: 0x7ff7bc8c42a8, ptr of data: [1, 2, 3, 4]
-//addr of value: 0x7ff7bc8c4290, addr of ref: 0x7ff7bc8c40f0
-//sum of data_owned: 10
-//addr of items: [0x60000316c050, 0x60000316c054, 0x60000316c058, 0x60000316c05c]
-
+//
+// addr of value: 0x7ff7bbc7d198(0x7ff7bbc7d198), addr of data 0x7ff7bbc7d270, data_owned: 0x7ff7bbc7d1b0, ptr of data: [1, 2, 3, 4]
+// addr of value: 0x7ff7bbc7d198, addr of ref: 0x7ff7bbc7cfc0
+// sum of data_owned: 10
+// addr of items: [0x600003ea4050, 0x600003ea4054, 0x600003ea4058, 0x600003ea405c]
+// stack value: 7ff7bbc7d198
