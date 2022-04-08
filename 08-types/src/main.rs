5037c7d94382ef9e766902a702c10687b6a7b6ed
Yan Qian <qianyan.lambda@gmail.com>
2022-04-08 21:38:58 +0800

    feat(type): type Error for trait

--- 08-types/src/main.rs
+++ 08-types/src/main.rs
@@ -2,7 +2,10 @@ use std::str::FromStr;
 use regex::Regex;
 
 pub trait Parse {
-    fn parse(s: &str) -> Self;
+    type Error;
+    fn parse(s: &str) -> Result<Self, Self::Error>
+        where
+            Self: Sized;
 }
 
 //impl Parse for u8 {
@@ -35,31 +38,32 @@ impl<T> Parse for T
 where 
     T: FromStr + Default,
 {
-    fn parse(s: &str) -> Self {
+    type Error = String;
+    fn parse(s: &str) -> Result<Self, Self::Error> {
         let re: Regex = Regex::new(r"^[0-9]+(\.[0-9]+)?").unwrap();
-
-        let d = || Default::default();
 
         if let Some(captures) = re.captures(s) {
             captures
                 .get(0)
-                .map_or(d(), |s| s.as_str().parse().unwrap_or(d()))
+                .map_or(Err("failed to capture".to_string()), |s| 
+                    {
+                        s.as_str()
+                            .parse()
+                            .map_err(|_err| "failed to parse captured string".to_string())
+                    })
         } else {
-            d()
+            Err("failed parse string".to_string())
         }
     }
 }
 
 #[test]
 fn parse_should_work() {
-    assert_eq!(u8::parse("123abcd"), 123);
-    // 越界了自动变成0
-    assert_eq!(u8::parse("1234abcd"), 0);
-    assert_eq!(u8::parse("abcd"), 0);
-
-    assert_eq!(f64::parse("123.45abcd"), 123.45);
+    assert_eq!(u8::parse("123abcd"), Ok(123));
+    assert_eq!(
+        u32::parse("123.45abcd"), 
+        Err("failed to parse captured string".into()));
+    assert_eq!(f64::parse("123.45abcd"), Ok(123.45));
+    assert!(f64::parse("abcd").is_err());
 }
 
-fn main() {
-    println!("result: {}", u8::parse("255 hello world"));
-}
