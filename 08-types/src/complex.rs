7fe9d0cded9a1901e019a288411b67e7807769eb
Yan Qian <qianyan.lambda@gmail.com>
2022-04-10 11:35:51 +0800

    feat(type): Add<Rhs = Self>

---
+++ 08-types/src/complex.rs
@@ -0,0 +1,31 @@
+use std::ops::Add;
+
+#[derive(Debug, PartialEq)]
+pub struct Complex {
+    real: f64,
+    imagine: f64,
+}
+
+impl Complex {
+    pub fn new(real: f64, imagine: f64) -> Self {
+        Self {real, imagine}
+    }
+}
+
+impl Add for Complex {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        let real = self.real + rhs.real;
+        let imagine = self.imagine + rhs.imagine;
+        Self::new(real, imagine)
+    }
+}
+
+#[test]
+fn override_add_for_complex() {
+    let c1 = Complex::new(1.0, 1f64);
+    let c2 = Complex::new(2 as f64, 3.0);
+    assert_eq!(c1 + c2, Complex::new(3.0, 4.0));
+}
+
