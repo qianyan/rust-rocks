abd1dddce486d02d98c76a1627d1ff741af7f4b0
Yan Qian <qianyan.lambda@gmail.com>
2022-04-10 15:48:52 +0800

    feat(type): Add<Rhs = Self> with reference

--- 08-types/src/complex.rs
+++ 08-types/src/complex.rs
@@ -21,11 +21,21 @@ impl Add for Complex {
         Self::new(real, imagine)
     }
 }
+// 为&Complex 实现 Add，如此可以实现 &c1 + &c2
+impl Add for &Complex {
+    type Output = Complex;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        let real = self.real + rhs.real;
+        let imagine = self.imagine + rhs.imagine;
+        Complex::new(real, imagine)
+    }
+}
 
 #[test]
 fn override_add_for_complex() {
     let c1 = Complex::new(1.0, 1f64);
     let c2 = Complex::new(2 as f64, 3.0);
-    assert_eq!(c1 + c2, Complex::new(3.0, 4.0));
+    assert_eq!(&c1 + &c2, Complex::new(3.0, 4.0));
 }
 
--- 08-types/src/main.rs
+++ 08-types/src/main.rs
@@ -7,5 +7,6 @@ fn main() {
     let c1 = Complex::new(1.0, 1f64);
     let c2 = Complex::new(2 as f64, 3.0);
 
+    println!("{:?}", &c1 + &c2);
     println!("{:?}", c1 + c2);
 }
