3b61b07e27b5e0093f9037e75661209e8fef87f9
Yan Qian <qianyan.lambda@gmail.com>
2022-09-19 09:05:37 +0800

    test: define TestResult for each test return

--- 09-cli/echor/tests/cli.rs
+++ 09-cli/echor/tests/cli.rs
@@ -3,53 +3,55 @@ use std::fs;
 use assert_cmd::Command;
 use predicates::prelude::*;
 
+type TestResult = Result<(), Box<dyn std::error::Error>>;
+// Box indicates that the error will live inside a kind of pointer where the memory is dynamically allocated on the heap rather than the stack.
+// dyn indicates that the method calls on the std::error:Error trait are dynamically dispatched.
+
 #[test]
-fn fails_no_args() {
-    let mut cmd = Command::cargo_bin("echor").unwrap();
+fn fails_no_args() -> TestResult {
+    let mut cmd = Command::cargo_bin("echor")?;
     cmd.assert()
         .failure()
         .stderr(predicate::str::contains("USAGE"));
+    Ok(())
 }
 
 #[test]
-fn ok_with_args() {
-    Command::cargo_bin("echor")
-        .unwrap()
-        .arg("hello")
-        .assert()
-        .success();
+fn ok_with_args() -> TestResult {
+    Command::cargo_bin("echor")?.arg("hello").assert().success();
+    Ok(())
 }
 
 #[test]
-fn echo_hello_world() {
+fn echo_hello_world() -> TestResult {
     let outfile = "tests/fixtures/hello_world.txt";
-    run(&["Hello World"], outfile);
+    run(&["Hello World"], outfile)
 }
 
 #[test]
-fn echo_hello_x_world() {
+fn echo_hello_x_world() -> TestResult {
     let outfile = "tests/fixtures/hello_world.spaces.txt";
-    run(&["Hello", "World"], outfile);
+    run(&["Hello", "World"], outfile)
 }
 
 #[test]
-fn echo_hello_x_world_no_newline() {
+fn echo_hello_x_world_no_newline() -> TestResult {
     let outfile = "tests/fixtures/hello_world.spaces.n.txt";
-    run(&["-n", "Hello", "World"], outfile);
+    run(&["-n", "Hello", "World"], outfile)
 }
 
 #[test]
-fn echo_hello_world_no_newline() {
+fn echo_hello_world_no_newline() -> TestResult {
     let outfile = "tests/fixtures/hello_world.n.txt";
-    run(&["Hello World", "-n"], outfile);
+    run(&["Hello World", "-n"], outfile)
 }
 
-fn run(args: &[&str], expected_file: &str) {
+fn run(args: &[&str], expected_file: &str) -> TestResult {
     let expected = fs::read_to_string(expected_file).unwrap();
-    Command::cargo_bin("echor")
-        .unwrap()
+    Command::cargo_bin("echor")?
         .args(args)
         .assert()
         .success()
         .stdout(expected);
+    Ok(())
 }
