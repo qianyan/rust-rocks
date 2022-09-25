065bf9a0fab77f4b126091b9b63a9ffd4f7f979b
Yan Qian <qianyan.lambda@gmail.com>
2022-09-25 12:47:37 +0800

    feat: print first 10 line by default

--- 09-cli/headr/src/lib.rs
+++ 09-cli/headr/src/lib.rs
@@ -1,10 +1,90 @@
-use std::error::Error;
+use std::{
+    error::Error,
+    fs::File,
+    io::{self, BufRead, BufReader},
+};
+
+use clap::{value_parser, Arg, Command};
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
 pub fn parse_positive_int(val: &str) -> MyResult<usize> {
     match val.parse() {
         Ok(n) if n > 0 => Ok(n),
-        _ => Err(val.into())
+        _ => Err(val.into()),
     }
+}
+
+#[derive(Debug)]
+pub struct Config {
+    files: Vec<String>,
+    lines: usize,
+    bytes: Option<usize>,
+}
+
+pub fn get_args() -> MyResult<Config> {
+    let _matches = Command::new("headr")
+        .version("0.1.0")
+        .author("qianyan qianyan.lambda@gmail.com")
+        .about("Rust head")
+        .arg(
+            Arg::with_name("files")
+                .value_name("FILE")
+                .help("Input file(s)")
+                .multiple_values(true)
+                .default_value("-"),
+        )
+        .arg(
+            Arg::with_name("lines")
+                .short('n')
+                .value_parser(value_parser!(usize))
+                .takes_value(true)
+                .default_value("10")
+                .help("print first n lines, 10 by default.")
+                .conflicts_with("bytes"),
+        )
+        .arg(
+            Arg::with_name("bytes")
+                .short('c')
+                .value_parser(value_parser!(usize))
+                .takes_value(true)
+                .help("print first c bytes"),
+        )
+        .get_matches();
+
+    let files: Vec<String> = _matches
+        .get_many::<String>("files")
+        .unwrap()
+        .map(|s| s.to_owned())
+        .collect();
+
+    let lines = _matches.get_one::<usize>("lines").unwrap().to_owned();
+    let bytes = _matches.get_one::<usize>("bytes").map(|b| b.to_owned());
+
+    Ok(Config {
+        files,
+        lines,
+        bytes,
+    })
+}
+
+fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
+}
+
+pub fn run(config: Config) -> MyResult<()> {
+    for filename in config.files {
+        match open(&filename) {
+            Err(err) => eprintln!("Failed to open {}: {}", filename, err),
+            Ok(reader) => {
+                for line in reader.lines().take(config.lines) {
+                    println!("{}", line?);
+                }
+            }
+        }
+    }
+    Ok(())
 }
--- 09-cli/headr/tests/cli.rs
+++ 09-cli/headr/tests/cli.rs
@@ -1,3 +1,9 @@
+use std::{error::Error, fs};
+
+use assert_cmd::Command;
+use predicates::prelude::predicate;
+
+const PRG: &str = "headr";
 #[test]
 fn test_parse_positive_int() {
     let res = headr::parse_positive_int("3");
@@ -11,4 +17,17 @@ fn test_parse_positive_int() {
     let res = headr::parse_positive_int("0");
     assert!(res.is_err());
     assert_eq!(res.unwrap_err().to_string(), "0".to_string());
+}
+
+type TestResult = Result<(), Box<dyn Error>>;
+#[test]
+fn print_the_first_10_lines_by_default() -> TestResult {
+    let expected = fs::read_to_string("tests/expected/ten.txt.out")?;
+    Command::cargo_bin(PRG)?
+        .args(["tests/inputs/ten.txt"])
+        .assert()
+        .success()
+        .stdout(expected);
+
+    Ok(())
 }
