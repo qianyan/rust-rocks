2c0513fa919c17d23bd3645659fbc943a989d252
Yan Qian <qianyan.lambda@gmail.com>
2022-09-25 12:49:42 +0800

    refactor: remove unused func

--- 09-cli/headr/src/lib.rs
+++ 09-cli/headr/src/lib.rs
@@ -8,13 +8,6 @@ use clap::{value_parser, Arg, Command};
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
-pub fn parse_positive_int(val: &str) -> MyResult<usize> {
-    match val.parse() {
-        Ok(n) if n > 0 => Ok(n),
-        _ => Err(val.into()),
-    }
-}
-
 #[derive(Debug)]
 pub struct Config {
     files: Vec<String>,
--- 09-cli/headr/tests/cli.rs
+++ 09-cli/headr/tests/cli.rs
@@ -1,23 +1,8 @@
 use std::{error::Error, fs};
 
 use assert_cmd::Command;
-use predicates::prelude::predicate;
 
 const PRG: &str = "headr";
-#[test]
-fn test_parse_positive_int() {
-    let res = headr::parse_positive_int("3");
-    assert!(res.is_ok());
-    assert_eq!(res.unwrap(), 3);
-
-    let res = headr::parse_positive_int("nonInteger");
-    assert!(res.is_err());
-    assert_eq!(res.unwrap_err().to_string(), "nonInteger".to_string());
-
-    let res = headr::parse_positive_int("0");
-    assert!(res.is_err());
-    assert_eq!(res.unwrap_err().to_string(), "0".to_string());
-}
 
 type TestResult = Result<(), Box<dyn Error>>;
 #[test]
